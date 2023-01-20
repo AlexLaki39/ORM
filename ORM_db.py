dcea40c309601f459b28f8007b37a6c843465c6d
Aleksandr Lakienko <sasha-lakienko5@mail.ru>
2023-01-20 14:47:08 +0300

    Initial commit

---
+++ ORM_db.py
@@ -0,0 +1,46 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import sessionmaker
+import json
+
+from models import create_tables, Publisher, Shop, Book, Stock, Sale
+
+# DSN = '...'
+engine = sq.create_engine(DSN)
+
+create_tables(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+with open('tests_data.json', 'r') as fd:
+    data = json.load(fd)
+
+for record in data:
+    model = {
+        'publisher': Publisher,
+        'shop': Shop,
+        'book': Book,
+        'stock': Stock,
+        'sale': Sale,
+    }[record.get('model')]
+    session.add(model(id=record.get('pk'), **record.get('fields')))
+session.commit()
+
+
+def get_info_buying_books(publisher_name):
+    if publisher_name.isnumeric():
+        for i in session.query(Book.title, Shop.name, Stock.count).\
+                join(Stock.shops).join(Stock.books).join(Book.publishers).\
+                filter(Publisher.id == int(publisher_name)):
+            print(i)
+    else:
+        for i in session.query(Book.title, Shop.name, Stock.count).\
+                join(Stock.shops).join(Stock.books).join(Book.publishers).\
+                filter(Publisher.name.like(f'{publisher_name}')):
+            print(i)
+
+session.close()
+
+if __name__ == '__main__':
+    publisher_name = input('Введите имя или id издателя: ')
+    get_info_buying_books(publisher_name)
---
+++ models.py
@@ -0,0 +1,63 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+
+class Publisher(Base):
+    __tablename__ = 'publisher'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=60), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'Издатель: id{self.id}, name {self.name}'
+
+
+class Book(Base):
+    __tablename__ = 'book'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    title = sq.Column(sq.Text, nullable=False)
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey('publisher.id'), nullable=False)
+
+    publishers = relationship(Publisher, backref='book')
+
+
+class Shop(Base):
+    __tablename__ = 'shop'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=40), unique=True)
+
+    def __repr__(self):
+        return f'Магазин: id{self.id}, name {self.name}'
+
+
+class Stock(Base):
+    __tablename__ = 'stock'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    id_book = sq.Column(sq.Integer, sq.ForeignKey('book.id'), nullable=False)
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey('shop.id'), nullable=False)
+    count = sq.Column(sq.Integer)
+
+    books = relationship(Book, backref='stock')
+    shops = relationship(Shop, backref='stock')
+
+
+class Sale(Base):
+    __tablename__ = 'sale'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    price = sq.Column(sq.FLOAT, nullable=False)
+    date_sale = sq.Column(sq.String, nullable=False)
+    id_stock = sq.Column(sq.Integer, sq.ForeignKey('stock.id'), nullable=False)
+    count = sq.Column(sq.Integer)
+
+    stocks = relationship(Stock, backref='sale')
+
+
+def create_tables(engine):
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
